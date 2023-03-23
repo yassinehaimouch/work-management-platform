774803e9c7bdee467b21e221320ce110e0997c19
yassinehaimouch <haymouch53@gmail.com>
2023-03-23 01:37:15 +0000

    [Style]: Styling Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import {
+  AppContainer,
+  CardContainer,
+  ColumContainer,
+  ColumnTitle,
+} from "./styles";
+
 export const App = () => {
-  return null;
+  return (
+    <AppContainer>
+      <ColumContainer>
+        <ColumnTitle>Todo :</ColumnTitle>
+        <CardContainer>First Item</CardContainer>
+        <CardContainer>Second Item</CardContainer>
+      </ColumContainer>
+    </AppContainer>
+  );
 };
